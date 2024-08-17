cf05a822d471b884dd7707155129977c2bc3fa4d
Eduardo Castro <136261834+ECastro08@users.noreply.github.com>
2024-08-17 17:06:56 -0500

    Resolviendo reto primera parte

---
+++ secondReto.js
@@ -0,0 +1,7 @@
+let inputName = prompt("Cual es su nombre");
+let inputAge = prompt("Cuántos años tienes?");
+let inputlenguaje = prompt("Lenguaje de programación");
+
+console.log(`Hola ${inputName}, tienes ${inputAge} y ya estás aprendiendo ${inputlenguaje}.`);
+
+
