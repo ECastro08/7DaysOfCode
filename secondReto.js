b693c2137c80b0c3942a817c898e5db1a2d76fe0
Eduardo Castro <136261834+ECastro08@users.noreply.github.com>
2024-08-18 12:06:26 -0500

    agregando opcional

--- secondReto.js
+++ secondReto.js
@@ -4,4 +4,12 @@ let inputlenguaje = prompt("Lenguaje de programación");
 
 console.log(`Hola ${inputName}, tienes ${inputAge} y ya estás aprendiendo ${inputlenguaje}.`);
 
+console.log(`Te gustaría estudiar ${inputlenguaje}? Responde 1 para SI y 0 para NO`);
+
+let answerUser = prompt("Ingresa tu respuesta");
+
+let ResultadoUser = (answerUser = 1) ? "¡Muy bien! Sigue estudiando y tendrás mucho éxito" : " Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?";
+
+console.log(resultadoUser);
+
 
