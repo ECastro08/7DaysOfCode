95d811b390d010bf82b4b82456431308ec2fe892
Eduardo Castro <136261834+ECastro08@users.noreply.github.com>
2024-08-19 19:39:20 -0500

    empezando challenge

---
+++ thirdReto.js
@@ -0,0 +1,21 @@
+console.log("Elige 1: Backend 2: Frontend");
+
+const programs = [
+  {name: "Frontend",
+   curses: ["React","vue"]
+  },
+  {
+   name:"Backend",
+   curses:["C#","Java"]
+  },
+  {
+   name:"Fullstack",
+   curses:[]
+  }
+]
+
+
+let chooseUser = prompt("Escoge 1 o 2");
+if (chooseUser = 1) {
+  console.log(`Escogiste aprender ${programas.name().value}`);
+}
